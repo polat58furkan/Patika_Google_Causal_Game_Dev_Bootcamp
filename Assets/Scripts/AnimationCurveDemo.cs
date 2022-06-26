02e00be34b82352e8203cb1b102f99bf8c6ec0b1
polat58furkan <polat58furkan@hotmail.com>
2022-06-26 14:48:32 +0300

    no message

---
+++ Assets/Scripts/AnimationCurveDemo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCurveDemo : MonoBehaviour
+{
+    [SerializeField] AnimationCurve _animationCurve;
+    [SerializeField] float animTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimationCurveCor());
+    }
+
+    // Update is called once per frame
+    IEnumerator AnimationCurveCor()
+    {
+        float currentTime =0f;
+        _animationCurve=new AnimationCurve(new Keyframe(0f,transform.position.x), new Keyframe(0.5f,transform.position.x+10), new Keyframe(1f,transform.position.x+15));
+
+        _animationCurve.SmoothTangents(0,0.25f);
+        _animationCurve.SmoothTangents(1,1f);
+        _animationCurve.SmoothTangents(2,0.25f);
+        while(currentTime<animTime)
+        {
+            transform.position=new Vector2(_animationCurve.Evaluate(currentTime/animTime),transform.position.y);
+            currentTime+=Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
